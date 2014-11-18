4ee39ad7cf916d580e3a32aca03ea4513ee9ef5b
anthonyb28 <anthony@lssclan.org>
2014-11-18 18:21:10 -0500

    Prototype chess board
    Basic, proposed 8x8 representation of a chess board using "Pieces"
    enums.

---
+++ ChessBoard.cs
@@ -0,0 +1,16 @@
+﻿/* Representation of a 8x8 chess board
+ * Each piece can be numbers 0-12 (enum of Pieces)
+ */
+
+namespace ChessAI
+{
+    class ChessBoard
+    {
+        public ChessBoard()
+        {
+            board = new Pieces[8, 8];
+        }
+
+        private Pieces[,] board;
+    }
+}
---
+++ Pieces.cs
@@ -0,0 +1,21 @@
+﻿// Representation of each type of pieces on a chess board (0-12)
+// Blank is considered a "piece", maybe rename to something else like tile?
+
+namespace ChessAI
+{
+    static enum Pieces
+    {
+        BLANK = 0,
+        WHITE_PAWN,
+        WHITE_KNIGHT,
+        WHITE_ROOK,
+        WHITE_BISHOP,
+        WHITE_QUEEN,
+        WHITE_KNIGHT,
+        BLACK_PAWN,
+        BLACK_KNIGHT,
+        BLACK_BISHOP,
+        WHITE_QUEEN,
+        WHITE_KNIGHT
+    }
+}
