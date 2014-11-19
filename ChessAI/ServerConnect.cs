ef98f04e48d2b85ab800a1e665a30813b94bb02a
Anthony Barranco <anthony@lssclan.org>
2014-11-18 21:34:13 -0500

    JSON Parse
    Quick and dirty JSON parse. Will have to figure out how this works async
    into the game and how to run it every 5 seconds.

---
+++ ChessAI/ServerConnect.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Runtime.Serialization.Json;
+
+namespace ChessAI
+{
+    class ServerConnect
+    {
+        
+        static void MakeRequest()
+        {
+            Uri serviceUri = new Uri("http://www.bencarle.com/chess/poll/GAMEID/TEAMNUMBER/TEAMSECRET/");
+            WebClient downloader = new WebClient();
+            downloader.OpenReadCompleted += new OpenReadCompletedEventHandler(RequestComplete);
+            downloader.OpenReadAsync(serviceUri);
+        }
+
+        static private void RequestComplete(object sender, OpenReadCompletedEventArgs e)
+        {
+            if (e.Error == null)
+            {
+                Stream responseStream = e.Result;
+                DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(JSONResponse));
+                JSONResponse response = (JSONResponse)serializer.ReadObject(responseStream);
+                if(response.ready)
+                {
+                    // Our turn
+                }
+                else
+                {
+                    // Opponent's turn
+                }
+            }
+        }
+
+        class JSONResponse
+        {
+            public bool ready { get; set;}
+            public float secondsleft { get; set; }
+            public int lastMoveNumber { get; set; }
+            public string lastMove { get; set; }
+        }
+    }
+}
